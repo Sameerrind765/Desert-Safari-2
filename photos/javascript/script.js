6be7806c2419078805dfa9cd1460f3f4fb62a128
Sameerrind765 <sameerrind789@gmail.com>
2024-05-18 11:54:22 -0700

    javascript functionality added

---
+++ photos/javascript/script.js
@@ -0,0 +1,25 @@
+// console.log("hekkiw")
+// // console.log(submit);    
+// let submit = document.getElementById("submit");
+// function clientInfo() {
+//     let clientName = document.getElementById("clientName").value;
+//     let clientNo = document.getElementById("clientNo").value;
+//     let clientEmail = document.getElementById("clientEmail").value;
+//     let pickupLocation = document.getElementById("pickupLocation").value;
+//     let NoOfGuests = document.getElementById("noOfGuests").value;
+//     let tourDate = document.getElementById("tourDate").value;
+//     let tourOption = document.getElementById("tourOption").value;
+//     let bookingInfo = {
+//         clientName,
+//         clientNo,
+//         pickupLocation,
+//         NoOfGuests,
+//         tourDate,
+//         tourOption,
+//         }
+//         console.log(bookingInfo);
+//     return bookingInfo
+// }
+// submit.addEventListener("click", () => {
+//     clientInfo();
+// });
---
+++ script.js
@@ -0,0 +1,11 @@
+function book(){
+    
+}
+function whatsapp(){
+    location.href = "https://wa.me/971544304980?text=%3Welcome%20To%20World!";
+}
+function call(){
+    location.href = "tel:+971544304980";
+}
+// whatsapp()
+// console.log();
