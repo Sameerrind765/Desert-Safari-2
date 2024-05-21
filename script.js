70e4e37ed60380cc9a2588879b6ce145fbd62c86
Sameerrind765 <sameerrind789@gmail.com>
2024-05-21 05:22:21 -0700

    whatsapp app link updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function book(){   
 }
 function whatsapp(){
-    location.href = "https://wa.me/971544304980?text=Welcome%20To%20World!";
+    location.href = "https://wa.me/971544304980";
 }
 function call(){
     location.href = "tel:+971544304980";
