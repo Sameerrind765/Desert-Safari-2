4996084752889d63156c5f8507452d787df8eb3f
Sameerrind765 <sameerrind789@gmail.com>
2024-06-03 01:24:07 -0700

    new photos

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function book(){   
+let bookbtn = document.getElementsByClassName("BuyButton");
+function book(scroll){   
+    scroll.scrollTo(1, 5000);
 }
 function whatsapp(){
     location.href = "https://wa.me/971544304980";
@@ -6,3 +8,6 @@ function whatsapp(){
 function call(){
     location.href = "tel:+971544304980";
 }
+// bookbtn.addEventListener("onclick", () => {
+//     book(bookbtn);
+// })
